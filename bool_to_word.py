c28eb0039643a1e7fbd1a450b8460b237996eead
hellwarrior-2 <feq_20@hotmail.com>
2020-01-02 17:26:31 -0500

    Kata 1 8kyu: Boolean to word

---
+++ bool_to_word.py
@@ -0,0 +1,10 @@
+#Exercise 8 kyu
+#Convert boolean values to strings 'Yes' or 'No'.
+
+#Description:
+#Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    return 'Yes' if True else 'No'
+    
+bool_to_word(True)
